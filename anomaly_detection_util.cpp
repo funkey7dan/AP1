7f4088419cfe9c2df597821fd99e4cfc2325d933
funkey7dan <72495653+funkey7dan@users.noreply.github.com>
2021-10-14 20:52:55 +0300

    First Upload, variance done

---
+++ anomaly_detection_util.cpp
@@ -0,0 +1,48 @@
+
+
+#include <cmath>
+#include "anomaly_detection_util.h"
+
+float mean(float *x, int size) {
+    float sum = 0;
+
+    for (int i = 0; i < size; ++i) {
+        sum += x[i];
+    }
+    return sum / size;
+}
+
+float var(float *x, int size) {
+    float mu = mean(x, size);
+    float sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += pow((x[i] - mu), 2);
+    }
+    return sum / size;
+}
+//
+//
+//// returns the covariance of X and Y
+//float cov(float *x, float *y, int size) {
+//
+//}
+//
+//// returns the Pearson correlation coefficient of X and Y
+//float pearson(float *x, float *y, int size) {
+
+//}
+//
+//// performs a linear regression and returns the line equation
+//Line linear_reg(Point **points, int size) {
+//}
+//
+//// returns the deviation between point p and the line equation of the points
+//float dev(Point p, Point **points, int size) {
+//
+//}
+//
+//// returns the deviation between point p and the line
+//float dev(Point p, Line l) {
+//
+//}
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "anomaly_detection_util.cpp"
+
+int main() {
+    float x[] = {1, 2, 3};
+    std::cout << var(x, 3);
+    return 0;
+}
