6dfd17dd32dd0c87deee773bd9517830fd8f7abf
TomerPardi <tomer.pardilov@live.biu.ac.il>
2021-10-15 12:39:16 +0300

    Implementation of cov function, added notes

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,29 +1,17 @@
+
+
 #include <cmath>
 #include "anomaly_detection_util.h"
 
-/**
- * Calculate the mean of a given range
- * @param x - array of floats
- * @param size - size of the sample
- * @return the mean of the range
- */
 float mean(float *x, int size) {
-    float sum = 0; // sum of the elements in the X array
+    float sum = 0;
 
-    // sums all the elements in the array
     for (int i = 0; i < size; ++i) {
         sum += x[i];
     }
-    // return the mean
     return sum / size;
 }
 
-/**
- * Calculate the variance of the given array
- * @param x array of floats
- * @param size size of the array
- * @return variance
- */
 float var(float *x, int size) {
     float mu = mean(x, size);
     float sum = 0;
@@ -32,19 +20,34 @@ float var(float *x, int size) {
     }
     return sum / size;
 }
+//
+//// returns the Pearson correlation coefficient of X and Y
+//float pearson(float *x, float *y, int size) {
 
+//}
+//
+//// performs a linear regression and returns the line equation
+//Line linear_reg(Point **points, int size) {
+=======
 /**
  * returns the covariance of X and Y
- * @param x
- * @param y
- * @param size
- * @return
+ * @param x array of floats.
+ * @param y array of floats.
+ * @param size size of arrays
+ * @return covariance between x and y.
  *
  */
 float cov(float *x, float *y, int size) {
-
+    //xm and ym are the means of two given arrays.
+    float xm = mean(x, size);
+    float ym = mean(y, size);
+    float sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += (x[i] - xm) * (y[i] - ym);
+    }
+    return sum / size;
 }
-//
+
 /**
  * returns the Pearson correlation coefficient of X and Y
  * @param x
@@ -52,10 +55,17 @@ float cov(float *x, float *y, int size) {
  * @param size
  * @return
  */
+<<<<<<< HEAD
+// @todo Implement pearson function
+//float pearson(float *x, float *y, int size) {
+//
+//}
+=======
 // TODO Implement pearson function
 float pearson(float *x, float *y, int size) {
 
 }
+>>>>>>> origin/master
 ////
 //
 /**
@@ -64,10 +74,16 @@ float pearson(float *x, float *y, int size) {
  * @param size
  * @return
  */
+<<<<<<< HEAD
+ //@todo Implement linear_reg function
+//Line linear_reg(Point **points, int size) {
+//}
+=======
  //TODO Implement linear_reg function
 Line linear_reg(Point **points, int size) {
 
 }
+>>>>>>> origin/master
 //
 /**
  * returns the deviation between point p and the line equation of the points
@@ -76,20 +92,41 @@ Line linear_reg(Point **points, int size) {
  * @param size
  * @return
  */
+<<<<<<< HEAD
+//@todo Implement dev function
+//float dev(Point p, Point **points, int size) {
+=======
 //TODO Implement dev function
 float dev(Point p, Point **points, int size) {
+>>>>>>> origin/master
 //
-}
+>>>>>>> origin/master
+//}
 //
+//// returns the deviation between point p and the line equation of the points
+//float dev(Point p, Point **points, int size) {
 //
+<<<<<<< HEAD
+//}
+//
+//// returns the deviation between point p and the line
+//float dev(Point p, Line l) {
+//
+=======
 /**
  * returns the deviation between point p and the line
  * @param p
  * @param l
  * @return
  */
+<<<<<<< HEAD
+// @todo Implement dev function
+//float dev(Point p, Line l) {
+=======
 // TODO Implement dev function
 float dev(Point p, Line l) {
+>>>>>>> origin/master
 //
-}
+>>>>>>> origin/master
+//}
 
