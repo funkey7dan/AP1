9ce86959f4317ce40614d3d785cdd352b8326c88
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2021-11-03 10:23:54 +0200

    implementation of timeseries.cpp and timeseries.h - need to be tested with csv file

--- timeseries.cpp
+++ timeseries.cpp
@@ -1,3 +1,78 @@
+#include <fstream>
+#include <sstream>
 #include "timeseries.h"
 
+// constructor
+TimeSeries::TimeSeries(const char *CSVfileName) {
+    filename = CSVfileName;
+    constructDataBase(data);
+}
 
+// getter
+const char *TimeSeries::getFileName() {
+    return filename;
+}
+
+// getter
+std::vector<std::pair<std::string, std::vector<float>>> TimeSeries::getDataBase() {
+    return data;
+}
+
+// get specific value from data
+float TimeSeries::getValueFromVector(int i, int j) {
+    return data.at(j).second.at(i);
+}
+
+// private method that is called from constructor only, the vector
+// is passed by reference to avoid copy.
+void TimeSeries::constructDataBase(vector<std::pair<std::string, std::vector<float>>> &data) {
+    // Reads a CSV file into a vector of <string, vector<int>> pairs where
+    // each pair represents <column name, column values>
+
+    // Create an input filestream
+    std::ifstream myFile(filename);
+
+    // Make sure the file is open
+    if (!myFile.is_open()) throw std::runtime_error("Could not open file");
+
+    // Helper vars
+    std::string line, colname;
+    float val;
+
+    // Read the column names
+    if (myFile.good()) {
+        // Extract the first line in the file
+        std::getline(myFile, line);
+
+        // Create a stringstream from line
+        std::stringstream ss(line);
+
+        // Extract each column name
+        while (std::getline(ss, colname, ',')) {
+
+            // Initialize and add <colname, int vector> pairs to data
+            data.push_back({colname, std::vector<float>{}});
+        }
+    }
+
+    // Read data, line by line
+    while (std::getline(myFile, line)) {
+        // Create a stringstream of the current line
+        std::istringstream ss(line);
+        std::string token;
+
+        // Keep track of the current column index
+        int colIdx = 0;
+
+        // Extract each float
+        while (std::getline(ss, token, ',')) {
+            // Add the current float-type to the 'colIdx' column's values vector
+            data.at(colIdx).second.push_back(std::stof(token));
+            // Increment the column index
+            colIdx++;
+        }
+
+        // Close file
+        myFile.close();
+    }
+}
--- timeseries.h
+++ timeseries.h
@@ -1,19 +1,27 @@
-
-
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
+
+#include <vector>
+#include <string>
 
 using namespace std;
 
 class TimeSeries{
+private:
+    const char* filename;
+    // we are going to store the data in vector of vectors while each
+    // vector is column in the file
+    std::vector<std::pair<std::string, std::vector<float>>> data;
+    // pass vector of vectors by reference - there we will read from file to the vector
+    // it is private because we will call it from the constructor only
+    void constructDataBase(std::vector<std::pair<std::string, std::vector<float>>>& data);
 
 public:
-
-	TimeSeries(const char* CSVfileName){
-	}
-
+    explicit TimeSeries(const char* CSVfileName);
+    const char* getFileName();
+    vector<pair<std::string, std::vector<float>>> getDataBase();
+    //row i column j
+    float getValueFromVector(int i, int j);
 };
-
-
 
 #endif /* TIMESERIES_H_ */
